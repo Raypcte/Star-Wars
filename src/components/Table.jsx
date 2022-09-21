24b8002770d78b12a9fad13d73d051661baf0ced
Raypcte <rayanevalak@gmail.com>
2022-09-21 00:04:30 -0300

    req1 questao capiroto

---
+++ src/components/Table.jsx
@@ -0,0 +1,55 @@
+import React, { useContext, useEffect } from 'react';
+import myContext from '../context/myContext';
+
+const Table = () => {
+  const { fetchApi, state } = useContext(myContext);
+
+  useEffect(() => {
+    fetchApi();
+  }, [fetchApi]); // Com array vazio, significa componentDidMount
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>name</th>
+          <th>rotation_period</th>
+          <th>orbital_period</th>
+          <th>diameter</th>
+          <th>climate</th>
+          <th>gravity</th>
+          <th>terrain</th>
+          <th>surface_water</th>
+          <th>population</th>
+          <th>films</th>
+          <th>created</th>
+          <th>edited</th>
+          <th>url</th>
+        </tr>
+
+      </thead>
+      <tbody>
+        { state.planetas.length > 0
+          && state.planetas.map((item) => (
+            <tr key={ item.name }>
+              <td>{item.name}</td>
+              <td>{item.rotation_period}</td>
+              <td>{item.orbital_period}</td>
+              <td>{item.diameter}</td>
+              <td>{item.climate}</td>
+              <td>{item.gravity}</td>
+              <td>{item.terrain}</td>
+              <td>{item.surface_water}</td>
+              <td>{item.population}</td>
+              <td>{item.films}</td>
+              <td>{item.created}</td>
+              <td>{item.edited}</td>
+              <td>{item.url}</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
---
+++ src/context/Provider.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import propTypes from 'prop-types';
+import MyContext from './myContext';
+
+const INITIAL_STATE = {
+  planetas: [],
+};
+
+function Provider({ children }) {
+  const [state, setState] = useState(INITIAL_STATE);
+
+  const fetchApi = async () => {
+    const planetas = await fetch('https://swapi.dev/api/planets');
+    const estrelas = await planetas.json();
+    setState({
+      planetas: estrelas.results,
+    });
+  };
+
+  const valoresGlobais = {
+    state, setState, fetchApi,
+  };
+
+  return (
+    <MyContext.Provider value={ valoresGlobais }>
+      {children}
+    </MyContext.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: propTypes.node.isRequired,
+};
+
+export default Provider;
